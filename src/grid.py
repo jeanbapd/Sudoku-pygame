4283669f4fdcb49d38e78e6fe107f5fbf9e1ae8e
Jean-baptiste <dylanj.jean-baptiste@grenoble-inp.org>
2025-12-26 19:44:09 +0100

    feat: Implement generate new game in Grid fix: Stack overflow in fill_grid

--- src/grid.py
+++ src/grid.py
@@ -16,3 +16,16 @@ class Grid:
         self.selected = None
 
         self.errors = []
+
+    def generate_new_game(self,difficulty="medium"):
+        """
+        Function to generate a new game
+        :param difficulty: The difficulty of the game
+        :return: None
+        """
+        self.grid = src.generator.generate_sudoku(difficulty)
+
+        self.original = src.generator.copy_grid(self.grid)
+
+        self.selected = None
+        self.errors = []
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -3,3 +3,13 @@ from src.grid import Grid
 print("Test 1: Creating new grid")
 grid = Grid()
 print("Grid created")
+
+print("Test 2: Create new game with different difficulty ")
+grid.generate_new_game("easy")
+print("Grid created")
+
+grid.generate_new_game("medium")
+print("Grid created")
+
+grid.generate_new_game("hard")
+print("Grid created")
